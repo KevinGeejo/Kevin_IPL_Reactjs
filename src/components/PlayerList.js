749c26b5c3318d4169a90ea81e3cb05c664aa04c
Kevin G <kevingeejo02@gmail.com>
2024-08-24 16:17:11 +0530

    ui changes 2

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -20,15 +20,15 @@ const PlayerList = () => {
     }, [])
 
     return <>
-    <div className="bg-[#0b0b13]">
+    <div className="bg-[#17171f] h-full">
 
     <Navbar/>
-    <div className="text-white m-5">
+    <div className="text-white m-5 h-full">
 
         <h1 className="text-white text-lg">List of Players Available {playerList?.length}</h1>
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center h-full">
             {playerList.map((player, index) => (
-                <div key={index} className="w-full md:w-1/2 xl:w-1/3 p-4">
+                <div key={index} className="w-full h-full md:w-1/2 xl:w-1/3 p-4">
                     <div className="bg-[#d5ad19] rounded shadow-md p-4">
                         <h2 className="text-black text-lg font-bold">{player.playerName}</h2>
                         <p className="text-gray-600">Team ID: {player.teamId}</p>
